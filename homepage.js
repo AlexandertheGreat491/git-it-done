d60138b1d03b74deb2cdd69e6a76162d6938ddce
agvan <agvandyke25@gmail.com>
2022-05-24 14:31:27 -0500

    added the fetch method to the getUserRepos function

--- homepage.js
+++ homepage.js
@@ -1,4 +1,7 @@
 var getUserRepos = function() {
-    console.log("function was called");
+    //console.log("function was called");
+    fetch("https://api.github.com/users/octocat/repos");
+    // Fetch API provides a specifc interface for fetching resoures.
 };
+
 getUserRepos();
