238a98e5d63e279843ec560abe97e98f6d75ea29
agvan <agvandyke25@gmail.com>
2022-05-24 15:20:28 -0500

    created the apiUrl variable in the getUserRepos variable to format the github api url & used the fetch, then, and json methods to request the url

--- homepage.js
+++ homepage.js
@@ -1,10 +1,14 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        response.json().then(function(data) {
-          console.log(data);
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
         });
-      });
-    };
+    });
+};
 
 // json() method formats the response as json.
 // then() method returns a Promise & takes up to 2 new arguments: callback functions for the success and failure cases of the Promise.
