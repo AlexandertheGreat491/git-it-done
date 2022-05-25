c526fec39e45015e80da4954fc31fd8adb43bf05
agvan <agvandyke25@gmail.com>
2022-05-25 10:33:59 -0500

    quicksave

--- homepage.js
+++ homepage.js
@@ -1,83 +1,93 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
-// These first two variables will help with handling form submissions.
-// formSubmitHandler will be executed upon a form submission browser event.
-var repoContainerEl = document.querySector("#repos-container");
+var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    //console.log(event);
-    var username = nameInputEl.value.trim();
 
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username.");
-    }
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
 };
 
-userFormEl.addEventListener("submit", formSubmitHandler);
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-var getUserRepos = function (user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    // make a request to the url
-
-    fetch(apiUrl).then(function(response){
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data){
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error: GitHub User Not Found");
-        }
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
     })
-    //.catch() is the Fetch API's way of handling network errors.
     .catch(function(error) {
-        // Notice this .catch() getting chained onto the end of the .then() method.
-        alert('Unable to connect to GitHub');
+      alert("Unable to connect to GitHub");
     });
 };
 
-// json() method formats the response as json.
-// then() method returns a Promise & takes up to 2 new arguments: callback functions for the success and failure cases of the Promise.
-// Promise object represents the eventual completion or failure of an asynchronous operation and its resulting value.
+var displayRepos = function(repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
 
-var displayRepos = function (repos, searchTerm) {
-    // check if api returned any repos
-    if (repos.length === 0) {
-        repoContainerEl.textContent = "No repositories found.";
-        return;
-    }
-    console.log(repos);
-    console.log(searchTerm);
-    // clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
-    // create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center"
+  repoSearchTerm.textContent = searchTerm;
 
-    // creates a span element & assigns flex-row & align-center classes to the span element.
-    // check if current repo has issues or not
-    // conditional statements that will create icons
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-            "<i class= 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
-    } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
     // append to container
-
     repoEl.appendChild(titleEl);
 
-    //append to container to the dom
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
 
-    repoContainerEl.appendChildd(statusEl);
-}
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
 
-getUserRepos();
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+// add event listeners to forms
+userFormEl.addEventListener("submit", formSubmitHandler);
