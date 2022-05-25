947c7d4ab75b6a79ca191dc8fddae6dd84019749
agvan <agvandyke25@gmail.com>
2022-05-24 21:18:48 -0500

    updated fetch() request to hand the server's response

--- single.js
+++ single.js
@@ -2,7 +2,16 @@ var getRepoIssues = function(repo) {
     console.log(repo);
     // format the URL
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl);
+    fetch(apiUrl).then(function(data){
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
 }
 
 getRepoIssues("facebook/react");
